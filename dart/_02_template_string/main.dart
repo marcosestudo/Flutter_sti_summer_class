b89e7405ce5b5f1ca8fb807b7dc084df54b691b5
marcos <estudogit@outlook.com>
2023-03-05 10:51:47 -0300

    comentario adicionado

--- dart/_02_template_string/main.dart
+++ dart/_02_template_string/main.dart
@@ -1,7 +1,7 @@
 // termplate string ou interpolação de strings
 
 main() {
-  var nome = "Marcos";
+  var nome = "Marcos"; // var establece o tipo da variável de acordo com a atribuição
   String sobrenome = "Machado"; // Declarando especificando o tipo
   print("Olá, ${nome} ${sobrenome}");
 }
