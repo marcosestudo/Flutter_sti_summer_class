a02513a2f7ab9ad07126cf238cd470d5022376be
marcos <estudogit@outlook.com>
2023-03-05 15:59:12 -0300

    comentario adicionado

--- dart/_02_template_string/main.dart
+++ dart/_02_template_string/main.dart
@@ -2,6 +2,7 @@
 
 main() {
   // var establece o tipo da variável de acordo com a atribuição
+  // Coloque o mause em cima do nome da variável para ver o tipo
   var nome = "Marcos";
   // Declaração especificando o tipo explicitamente
   String sobrenome = "Machado";
