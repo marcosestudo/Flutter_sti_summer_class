f90b673622892c75bf93c66a2c14fd7368b18675
marcos <estudogit@outlook.com>
2023-03-05 15:43:47 -0300

    null check

--- dart/_03_atribuicao_opcional/main.dart
+++ dart/_03_atribuicao_opcional/main.dart
@@ -3,4 +3,11 @@ main() {
   // Devemos usar o "?" pra dizer que a varável pode ser null
   String? semNome; // Sem o "?", se não atribuir valor a variável, dá erro
   print(semNome);
+  
+  // Podemos atribuirs a variável nula a uma outra variável com unm teste
+  // se a variável estiver nula na hora da atribuição, será atribuído o que
+  // está dpoutro lado do "??"
+  String nome = semNome ?? "sem nome";
+  print(nome);
+  print(semNome);
 }
