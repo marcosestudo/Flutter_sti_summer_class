140d4fc0521bc7a99614c240401f3456a7c2c469
marcos <estudogit@outlook.com>
2023-03-05 15:34:10 -0300

    tipos

---
+++ dart/_05_tipos/main.dart
@@ -0,0 +1,22 @@
+/*
+https://dart.dev/guides/language/language-tour#built-in-types
+
+Built-in types
+The Dart language has special support for the following:
+
+Numbers (int, double)
+Strings (String)
+Booleans (bool)
+Lists (List, also known as arrays)
+Sets (Set)
+Maps (Map)
+Runes (Runes; often replaced by the characters API)
+Symbols (Symbol)
+The value null (Null)
+*/
+
+void main() {
+  String nome = "Marcos";
+  int num1 = 53;
+  double num2 = 5.3;
+}
