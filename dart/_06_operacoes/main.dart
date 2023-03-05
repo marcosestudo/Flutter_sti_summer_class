b51476c3e3f5105c95cb51d13558a95271b13808
marcos <estudogit@outlook.com>
2023-03-05 17:17:37 -0300

    operacoes

---
+++ dart/_06_operacoes/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  print(5 + 3); // Soma
+  print(5 - 3); // Subtração
+  print(5 * 3); // Multiplicação
+  print(5 / 3); // Divisão
+  print(5 ~/ 3); // Divisão inteira
+  print(5 % 3); // mod ou resto da divisão
+}
