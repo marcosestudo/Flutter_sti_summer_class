e6f1dca528d8ca6c41f960330bcb616b0bf6e011
marcos <estudogit@outlook.com>
2023-03-05 11:10:17 -0300

    atribuiicao opcional

--- dart/_02_template_string/main.dart
+++ dart/_02_template_string/main.dart
@@ -1,7 +1,7 @@
-// termplate string ou interpolação de strings
+// template string ou interpolação de strings
 
 main() {
   var nome = "Marcos"; // var establece o tipo da variável de acordo com a atribuição
-  String sobrenome = "Machado"; // Declarando especificando o tipo
-  print("Olá, ${nome} ${sobrenome}");
+  String sobrenome = "Machado"; // Declaração especificando o tipo explicitamente
+  print("Olá, ${nome} ${sobrenome}!");
 }
---
+++ dart/_03_atribuicao_opcional/main.dart
@@ -0,0 +1,6 @@
+main() {
+  // null
+  // Devemos usar o "?" pra dizer que a varável pode ser null
+  String? semNome; // Sem o "?", se não atribuir valor a variável, dá erro
+  print(semNome);
+}
