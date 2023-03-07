f2fdcec5eb376a564a47180367e4451d03374d00
marcos <estudogit@outlook.com>
2023-03-07 20:52:07 -0300

    algumas funcoes

--- dart/_08_lista_ou_array/main.dart
+++ dart/_08_lista_ou_array/main.dart
@@ -1,6 +1,5 @@
-import 'dart:svg';
-
 void main() {
+  // se não especificar o tipo, podemos misturar
   List tudoJunto = ["A", "B", "C", 1, 2, 3];
   List<String> letras = ["A", "B", "C"];
   List<int> nums = [1, 2, 3];
@@ -9,4 +8,14 @@ void main() {
   // acesso aos elementos por colchetes
   print(tudoJunto[0]);
   print(tudoJunto[3]);
+
+  letras.add("D");
+  print(letras);
+
+  nums.removeLast();
+  print(nums);
+
+  // digite o nome da lista + ponto pra ver as funções disponíveis 
+  // ex: "letras." 
+  // o vscode vai mostrar as sugestões de funções
 }
