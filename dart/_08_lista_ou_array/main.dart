e29ead374851b70109de23e46bbd1785cf880732
marcos <estudogit@outlook.com>
2023-03-07 20:40:42 -0300

    listas

---
+++ dart/_08_lista_ou_array/main.dart
@@ -0,0 +1,12 @@
+import 'dart:svg';
+
+void main() {
+  List tudoJunto = ["A", "B", "C", 1, 2, 3];
+  List<String> letras = ["A", "B", "C"];
+  List<int> nums = [1, 2, 3];
+
+  print(tudoJunto);
+  // acesso aos elementos por colchetes
+  print(tudoJunto[0]);
+  print(tudoJunto[3]);
+}
