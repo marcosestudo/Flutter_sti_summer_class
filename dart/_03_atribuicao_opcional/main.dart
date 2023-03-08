bd14a9f719f64bf3cc15777789a8efbe570d342e
marcos <estudogit@outlook.com>
2023-03-08 09:29:34 -0300

    comentario adicionado

--- dart/_03_atribuicao_opcional/main.dart
+++ dart/_03_atribuicao_opcional/main.dart
@@ -4,9 +4,9 @@ main() {
   String? semNome; // Sem o "?", se não atribuir valor a variável, dá erro
   print(semNome);
   
-  // Podemos atribuirs a variável nula a uma outra variável com unm teste
+  // Podemos atribuir a variável nula a uma outra variável com um teste
   // se a variável estiver nula na hora da atribuição, será atribuído o que
-  // está dpoutro lado do "??"
+  // está do outro lado do "??"
   String nome = semNome ?? "sem nome";
   print(nome);
   print(semNome);
