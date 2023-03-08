d60702f7f4b207fb5c3c4fc311a2d5f8af6da8d9
marcos <estudogit@outlook.com>
2023-03-07 22:28:46 -0300

    map

---
+++ dart/_09_map/main.dart
@@ -0,0 +1,20 @@
+// maps são como obejtos javascript
+void main() {
+  Map gasesNobres = {
+    // chave: valor
+    2: "helio",
+    "neon": 10, // se não estiver especificado, podemos misturar os tipos
+    18: "argônio",
+  };
+  print(gasesNobres);
+  print(gasesNobres[2]);
+
+  // especificando os tipos do map, devemos especificar os dois
+  Map<String, double> letraNum = {
+    "A": 1.0,
+    "B": 2.0,
+    "C": 3.0,
+  };
+  print(letraNum);
+  print(letraNum["A"]);
+}
