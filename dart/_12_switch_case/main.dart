b573f78877b45419303bb577faeb1d7aca0cac67
marcos <estudogit@outlook.com>
2023-03-08 10:19:05 -0300

    switch case

---
+++ dart/_12_switch_case/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  String num = "Dois";
+
+  switch (num) {
+    case "Um":
+      print(1);
+      break;
+    case "Dois":
+      print(2);
+      break;
+    case "Três":
+      print(3);
+      break;
+    default:
+      print("Nenhum dos três");
+  }
+}
