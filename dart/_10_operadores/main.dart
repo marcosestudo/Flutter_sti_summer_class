1df48eb0ec3df6a82b52d3ffe1950a0ef5bef946
marcos <estudogit@outlook.com>
2023-03-08 09:19:02 -0300

    operadores

---
+++ dart/_10_operadores/main.dart
@@ -0,0 +1,21 @@
+// Operadores aritméticos
+// >  maior
+// >= maior ou igual
+// <  menor
+// <= menor ou igual
+// == igual
+// != diferente
+
+// Operadores lógicos
+// && e
+// || ou
+
+// Operadores de atribuição
+// ??= se a variável à esquerda do operador for nula, atribui o que está à direta
+// =	
+// *=	
+// %=	
+// +=	
+// /=	
+// -=	
+// ~/=	
