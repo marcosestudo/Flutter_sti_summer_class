8086443c9af43b40107d233c5233ac8ab960a3ea
marcos <estudogit@outlook.com>
2023-03-09 12:41:54 -0300

    iterando lista com for

--- dart/13_for/main.dart
+++ dart/13_for/main.dart
@@ -1,11 +1,12 @@
 void main() {
+  List<String> letras = ["A", "B", "C"];
+  
   for (int i = 0; i < 3; i++) {
-    print(i);
+    print(letras[i]);
   }
 
   print("---------------");
 
-  List<String> letras = ["A", "B", "C"];
   for (String letra in letras) {
     print(letra);
   }
