1febac88a18348dc3c99b6f520c028a8bedb107e
marcos <estudogit@outlook.com>
2023-03-09 12:32:30 -0300

    for in

--- dart/13_for/main.dart
+++ dart/13_for/main.dart
@@ -2,4 +2,11 @@ void main() {
   for (int i = 0; i < 3; i++) {
     print(i);
   }
+
+  print("---------------");
+
+  List<String> letras = ["A", "B", "C"];
+  for (String letra in letras) {
+    print(letra);
+  }
 }
